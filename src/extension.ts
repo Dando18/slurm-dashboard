575299bab5abf3158b83c6945bc40cca462b494d
Daniel Nichols <dando18studios@gmail.com>
2023-12-12 15:39:43 -0500

    several updates

--- src/extension.ts
+++ src/extension.ts
@@ -11,5 +11,4 @@ export function activate(context: vscode.ExtensionContext) {
 	new JobScriptProvider(scheduler).register(context);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
