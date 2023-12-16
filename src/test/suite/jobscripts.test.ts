a07c3df6a856ff55a2521932d5ff8d1b36772bd9
Daniel Nichols <dando18studios@gmail.com>
2023-12-16 13:02:51 -0500

    update job script tests

--- src/test/suite/jobscripts.test.ts
+++ src/test/suite/jobscripts.test.ts
@@ -19,4 +19,31 @@ suite('jobscripts.ts tests', () => {
                 assert.strictEqual(jobScript.description?.toString().slice(1), 'job1.sbatch');
             }
         });
+
+        test('JobScriptProvider :: constructor', () => {
+            const scheduler = new Debug();
+            assert.doesNotThrow(() => {
+                new jobscripts.JobScriptProvider(scheduler);
+            });
+        });
+
+        test('JobScriptProvider :: getTreeItem', () => {
+            const scheduler = new Debug();
+            const provider = new jobscripts.JobScriptProvider(scheduler);
+            const fpath = vscode.Uri.file('job1.sbatch');
+            const jobScript = new jobscripts.JobScript(fpath);
+            assert.strictEqual(provider.getTreeItem(jobScript), jobScript);
+        });
+
+        test('JobScriptProvider :: getChildren', async () => {
+            const scheduler = new Debug();
+            const provider = new jobscripts.JobScriptProvider(scheduler);
+            
+            let scripts = await provider.getChildren();
+
+            assert.ok(scripts);
+            assert.strictEqual(scripts.length, 3);
+            assert.ok(scripts[0] instanceof jobscripts.JobScript);
+            assert.strictEqual(scripts.filter((script) => script.label === 'job1.sbatch').length, 1);
+        });
 });
