78df0090dd9f1371705fdac9cc22cacde4c34197
Daniel Nichols <dando18studios@gmail.com>
2024-12-04 12:10:28 -0500

    fix formatting

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -13,7 +13,7 @@ export function run(): Promise<void> {
     const testsRoot = path.resolve(__dirname, '..');
 
     return new Promise((c, e) => {
-        globSync('**/**.test.js', { cwd: testsRoot }).forEach( (file) => {
+        globSync('**/**.test.js', { cwd: testsRoot }).forEach(file => {
             mocha.addFile(path.resolve(testsRoot, file));
         });
 
